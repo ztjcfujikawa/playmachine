e87928cf94faab6351ba9f204711c4b865ebfc5b
dreamhartley <zerxo123@gmail.com>
2025-03-31 21:45:13 +0800

    Update style

--- public/admin/script.js
+++ public/admin/script.js
@@ -242,25 +242,21 @@ function hideError(container = errorMessageDiv) {
                         <p class="text-xs text-gray-500">ID: ${key.id} | ${key.keyPreview}</p>
                     </div>
                     <div class="text-xs px-2 py-1 bg-blue-100 text-blue-800 rounded-full">
-                        Total: ${key.usage}
+                        <p class="text-xs text-gray-500">ID: ${key.id} | ${key.keyPreview}</p>
+                    </div>
+                    <div class="right-indicator flex items-center">
+                        ${key.errorStatus === 401 || key.errorStatus === 403 ? `
+                            <svg class="w-5 h-5 text-yellow-500 warning-icon" title="Error Status: ${key.errorStatus}" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 3.001-1.742 3.001H4.42c-1.53 0-2.493-1.667-1.743-3.001l5.58-9.92zM10 13a1 1 0 110-2 1 1 0 010 2zm0-8a1 1 0 011 1v3a1 1 0 11-2 0V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                            </svg>
+                        ` : `
+                            <div class="text-xs px-2 py-1 bg-blue-100 text-blue-800 rounded-full">
+                                Total: ${key.usage}
+                            </div>
+                        `}
                     </div>
                 </div>
             `;
-            // --- Add Warning Icon if errorStatus is present ---
-            let warningIconHTML = '';
-            if (key.errorStatus === 401 || key.errorStatus === 403) {
-                warningIconHTML = `
-                    <svg class="w-5 h-5 text-yellow-500 ml-2 warning-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 3.001-1.742 3.001H4.42c-1.53 0-2.493-1.667-1.743-3.001l5.58-9.92zM10 13a1 1 0 110-2 1 1 0 010 2zm0-8a1 1 0 011 1v3a1 1 0 11-2 0V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                    </svg>
-                `;
-            }
-            // --- End Warning Icon ---
-
-            // Add warning icon to the right side div
-            cardItem.querySelector('.flex.items-center.justify-between > div:last-child').insertAdjacentHTML('beforebegin', warningIconHTML);
-            // --- End Add Warning Icon ---
-
 
             keysGrid.appendChild(cardItem);
 
