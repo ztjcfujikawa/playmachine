0da87d51643e0a39f2624e6c1aa68abbb325ded7
DreamingZack <zerxo123@gmail.com>
2025-05-21 02:06:18 +0900

    Add gemini-2.5-flash-preview-05-20

--- src/services/vertexProxyService.js
+++ src/services/vertexProxyService.js
@@ -14,7 +14,8 @@ const VERTEX_SUPPORTED_MODELS = [
     "[v]gemini-2.5-pro-exp-03-25",
     "[v]gemini-2.5-pro-preview-05-06",
     "[v]gemini-2.0-flash-001",
-    "[v]gemini-2.5-flash-preview-04-17"
+    "[v]gemini-2.5-flash-preview-04-17",
+    "[v]gemini-2.5-flash-preview-05-20"
 ];
 
 // Default region
